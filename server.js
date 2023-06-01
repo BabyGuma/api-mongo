4feb268a6e65c0a3e576722b9abf85625f8af078
Angel Guarneros <111380789+BabyGuma@users.noreply.github.com>
2023-05-31 23:55:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 mongoose.connect('mongodb+srv://guma:12345@cludd.1p2lgqb.mongodb.net/',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  dbName: 'python' // Reemplaza con el nombre real de tu base de datos
+  dbName: 'memes' // Reemplaza con el nombre real de tu base de datos
 });
 
 // Middleware para analizar datos JSON en las solicitudes
